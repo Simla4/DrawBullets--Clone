b58a0f4f7bb96a6f09374fc17a1f26426794d650
Simla Beken <38881186+Simla4@users.noreply.github.com>
2022-05-09 14:10:37 +0300

    Added: win, lose and animations

--- DrawBullets/Assets/Scripts/Player/PlayerCollision.cs
+++ DrawBullets/Assets/Scripts/Player/PlayerCollision.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     [SerializeField] PlayerMovement playerMovement;
+    [SerializeField] GameObject enemy;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Enemy"))
         {
-            Debug.Log("Collision detected");
+            Debug.Log("Failed");
             playerMovement.speed = 0;
+        }
+        else if(other.gameObject.CompareTag("Finish"))
+        {
+            Debug.Log("Win");
+            playerMovement.speed = 0;
+            enemy.SetActive(false);
         }
     }
 }
